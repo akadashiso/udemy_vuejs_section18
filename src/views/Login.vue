9ab2f8e69d3284c575af088a4c4bff07f213bd92
akadashiso <akadashiso@gmail.com>
2021-05-17 23:20:19 +0900

    [Finish]Section18-244 : ログイン認証を行うために、Firebase Authenticationとプロジェクトのセットアップをする

---
+++ src/views/Login.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h2>ログイン</h2>
+    <label for="email">Email：</label>
+    <input
+      id="email"
+      type="email"
+      v-model="email"
+    >
+    <br><br>
+    <label for="name">パスワード：</label>
+    <input
+      id="password"
+      type="password"
+      v-model="password"
+    >
+    <br><br>
+    <button @click="Login">送信</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    login() {}
+  }
+};
+</script>
